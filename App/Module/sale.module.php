8f89e6457c6c78f39c9e38b3839f1c71d1a58ef7
Jeany <Jeany@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2015-11-10 02:03:48 +0000

    前端文字数字截取
    git-svn-id: http://192.168.0.5/svn/Member/trade/web@4956 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Module/sale.module.php
+++ App/Module/sale.module.php
@@ -62,17 +62,42 @@ class SaleModule extends AppModule
 		$r['raw']  = "status not in(2,3,4,6)";
 		$r['page']        	= $page;
         $r['limit']         = $num;
+		$r['col']           = array('name,class,id,source');
         $r['order']         = array('type' => 'asc','date' => 'desc');
         $data = $this->import('sale')->findAll($r);
         foreach($data['rows'] as $k => $item){
-			$data['rows'][$k]['imgurl'] = $this->load('saletrademark')->getImg($item['id']); 
-			$data['rows'][$k]['url'] = "/trademark/view/?id=".$item['id']."&c=".$c; 
+			$data['rows'][$k]['imgurl'] = $this->getImg($item['id']); 
+			$data['rows'][$k]['url']    = "/trademark/view/?id=".$item['id']."&c=".$c; 
+			$data['rows'][$k]['name']   = mbSub($item['name'],0,4); 
             $data['rows'][$k]['source'] = isset( $this->source[$item['source']] ) ? $this->source[$item['source']] : $item['source'];
 			$data['rows'][$k]['classes']  = isset( $this->classes[$item['class']] ) ? $this->classes[$item['class']] : $item['class'];
 			$data['notId'][] = $item['id'];
         }
 		return $data;
 	}
-
+	
+	/**
+	 * 出售详细信息图片信息
+	 * @author	Jeany
+	 * @since	2015-09-15
+	 *
+	 * @access	public
+	 * @param	array		$param  用户名称
+	 * @return	array
+	 */
+	public function getImg($id)
+	{	
+		$r['limit']           = 1;
+        $r['eq']['saleId']    = $id;
+		$r['col']             = array('imgurl');
+        $data                 = $this->import("saletrademark")->find($r);
+		if($data['imgurl']){
+			if(false===strpos(strtolower($data['imgurl']), 'http://')){
+				$data['imgurl'] = C('IMAGE_HOST').$data['imgurl']; 
+			}
+		}
+        return $data['imgurl'];
+	}
+	
 }
 ?>
