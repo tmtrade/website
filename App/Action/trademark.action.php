a97cc604c050f16f49067a0b5f92fd52de20961d
Jeany <Jeany@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2015-11-11 07:21:18 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/web@4976 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Action/trademark.action.php
+++ App/Action/trademark.action.php
@@ -18,7 +18,20 @@ class TrademarkAction extends AppAction
 	
 	public function view()
 	{
+		$id = "4497";
+		//$number     = $this->input("number","sting");
+		//$data       = $this->load("trademark")->trademarks($number);
+        $data    = $this->load("sale")->getDetail($id);
+		$detail  = $this->load("sale")->getTrademarkDetail($id);
 		
+		$this->set("platformIn",C('PLATFORM_IN'));
+		
+		//读取推荐商标
+		$tj  = $this->load("sale")->getDetailtj($data['class'],6,$data['id']);
+		
+		$this->set("data",$data);
+		$this->set("detail",$detail);
+		$this->set("tj",$tj);
 		$this->display();
 	}
 }
