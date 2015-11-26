ac7a354006a5e97aea86a0c3c47c238924995462
martin <martin@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2015-11-26 03:48:12 +0000

    帮组中心
    git-svn-id: http://192.168.0.5/svn/Member/trade/web@5563 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

---
+++ App/Action/faq.action.php
@@ -0,0 +1,38 @@
+<?
+/**
+ * 快速成交
+ *
+ * 网站首页
+ *
+ * @package Action
+ * @author  Xuni
+ * @since   2015-11-05
+ */
+class FaqAction extends AppAction
+{
+	
+	//公司介绍
+	public function protocol()
+    {
+        $this->display();
+    }
+
+	//常见问题
+	public function question()
+    {
+        $this->display();
+    }
+
+	//流程
+	public function process()
+    {
+        $this->display();
+    }
+
+	//规则
+	public function rule()
+    {
+        $this->display();
+    }
+}
+?>
