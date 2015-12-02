103609ebb29d9c6629dd5e53cce61f801d9f2b5a
martin <martin@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2015-12-02 08:54:08 +0000

    专利频道页
    git-svn-id: http://192.168.0.5/svn/Member/trade/web@5825 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

---
+++ App/Action/pt.action.php
@@ -0,0 +1,16 @@
+<?
+/**
+ * 快速成交
+ *
+ * @package Action
+ * @author  Martin
+ * @since   2015/12/2
+ */
+class PtAction extends AppAction
+{
+	public function index()
+    {
+        $this->display();
+    }
+}
+?>
