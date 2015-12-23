8eb2718564ff7ddc2371fa56a045e7b5ff51b217
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2015-12-23 10:15:44 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/web@6236 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Entity/sale.api.php
+++ App/Entity/sale.api.php
@@ -9,7 +9,7 @@
  * @version 3.0
  +------------------------------------------------------------------------------
  */
-class SaleApi extends Entity
+class SaleApi extends BaseCacheApi
 {
 	/**
 	 * 数据表键[表的唯一标识]
@@ -20,5 +20,16 @@ class SaleApi extends Entity
 	 * 数据表主键
 	 */
 	public $pk       = 'id';
+
+    /**
+     * 缓存组件标识id
+     */
+    public $cacheId = 'redis';
+
+    /**
+     * 过期时间(60分钟)
+     */
+    public $expire  = 3600;
+
 }
 ?>
