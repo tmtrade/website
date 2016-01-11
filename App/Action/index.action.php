99d49bc0839165eb276e4f243f6d37397d611fc1
martin <martin@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-01-11 07:53:47 +0000

    修改bug 新闻栏目更换
    git-svn-id: http://192.168.0.5/svn/Member/trade/web@6660 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Action/index.action.php
+++ App/Action/index.action.php
@@ -63,8 +63,8 @@ class IndexAction extends AppAction
 			'isBargain' => '2',
 			);
 		$offprice	= $this->load('search')->getSaleList($params, 0, 4);
-		$news['page']	= $this->load('faq')->newsList(45, 0, 5);
-		$news['faq']	= $this->load('faq')->newsList(47, 0, 5);
+		$news['page']	= $this->load('faq')->newsList(50, 0, 5);
+		$news['faq']	= $this->load('faq')->newsList(45, 0, 5);
 		$this->set('offpriceList', $this->load('sale')->getIndexOffprice());
 		
 		$this->set('dataTM',$dataTM);
