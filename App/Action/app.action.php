ffe1b2075713e720e20dacbba6ab08094e592a93
Jeany <Jeany@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-01-14 05:37:19 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/web@6804 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Action/app.action.php
+++ App/Action/app.action.php
@@ -23,7 +23,7 @@ abstract class AppAction extends Action
 
 	public $pageTitle 	= '一只蝉：百万注册商标转让平台网-独家100%签定风险赔付协议';
 	
-	public $pageKey 	= '商标转让,商标转让网,注册商标转让,转让商标,商标买卖,商标交易,商标交易网';
+	public $pageKey 	= '一只蝉,商标转让,商标转让网,注册商标转让,转让商标,商标买卖,商标交易,商标交易网';
 	
 	public $pageDescription 	= '一只蝉是超凡集团资产交易平台：13年积累约200余万商标转让信息-也是中国独家签订交易损失赔付协议保障风险平台-商标转让-专利转让';
 
