12e8ab4627d0ee3e15a01dc93171aedd1524b72b
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-01-25 01:33:47 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/web@7172 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Action/index.action.php
+++ App/Action/index.action.php
@@ -10,9 +10,9 @@
  */
 class IndexAction extends AppAction
 {
-	//public $caches  	= array('index');
-	//public $cacheId  	= 'redis';
-	//public $expire  	= 3600;//1小时
+	public $caches  	= array('index');
+	public $cacheId  	= 'redis';
+	public $expire  	= 3600;//1小时
 
 	public function index()
 	{
