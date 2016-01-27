7e562a6e6a099ad424000f917e8a5508bd9a84ee
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-01-27 10:24:24 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/web@7278 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Action/trademark.action.php
+++ App/Action/trademark.action.php
@@ -19,14 +19,15 @@ class TrademarkAction extends AppAction
 			$this->redirect('未找到页面', '/index/error/');
 		}
 
-		$info = $this->load('trademark')->getInfo($tid, array('`id` as `number`','`class`'));
-		if ( empty($info) || empty($info['number']) ){
-			$this->redirect('未找到页面1', '/index/error/');
-		}elseif ( !in_array($class, range(1,45)) || $class != $_info['class'] ){
+		//$info = $this->load('trademark')->getInfo($tid, array('`id` as `number`','`class`'));
+		//if ( empty($info) || empty($info['number']) ){
+		//	$this->redirect('未找到页面1', '/index/error/');
+		//}else
+		if ( !in_array($class, range(1,45))  ){
 			$this->redirect('未找到页面2', '/index/error/');
 		}
 
-		$this->redirect('', '/goods-'.$info['number'].'.html');
+		$this->redirect('', '/d-'.$tid.'-'.$class.'.html');
 	}
 
 }
