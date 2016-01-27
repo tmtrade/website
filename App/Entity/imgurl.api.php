e48e1b022f39c3b6664d378da4690c3a0410c0e5
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-01-27 08:18:05 +0000

    默认商标图片走缓存
    git-svn-id: http://192.168.0.5/svn/Member/trade/web@7267 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Entity/imgurl.api.php
+++ App/Entity/imgurl.api.php
@@ -9,7 +9,7 @@
  * @version 3.0
  +------------------------------------------------------------------------------
  */
-class ImgurlApi extends Entity
+class ImgurlApi extends BaseCacheApi
 {
 	/**
 	 * 数据表键[表的唯一标识]
@@ -20,5 +20,15 @@ class ImgurlApi extends Entity
 	 * 数据表主键
 	 */
 	public $pk       = 'auto';
+    
+    /**
+     * 缓存组件标识id
+     */
+    public $cacheId = 'redis';
+
+    /**
+     * 过期时间(60分钟)
+     */
+    public $expire  = 600;
 }
 ?>
