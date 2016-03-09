58c8347e5ec5cb570f7fea29f3a6c5d474331c3a
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-03-09 11:07:57 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/web@8107 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

---
+++ Static/1.2/js/menu_hide.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dower on 2016/3/9 0009.
+ */
+$(function(){
+    $('.mj-dd').hide();
+});
+$('.mj-dd').mousemove(function(){
+    $('.mj-dd').show();
+});
+$('.mj-dd').mouseleave(function(){
+    $('.mj-dd').hide();
+});
