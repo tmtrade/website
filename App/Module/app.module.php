96acb33bfc83a0468a61f13a6bcb020639e2837e
void <void@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-03-16 06:45:06 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/web@8315 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Module/app.module.php
+++ App/Module/app.module.php
@@ -42,6 +42,7 @@ abstract class AppModule extends Module
 		$className      = $name.'Bi';
 		$bi             = new $className();
 		$bi->url        = $config[$bi->apiId]['url'];
+		$bi->token      = $config[$bi->apiId]['token'];
 		$objList[$name] = $bi;
 		
 		return $bi;
