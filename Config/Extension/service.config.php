68bf58e8f3bed42c140fb9d1eda0d77a62a5b40d
void <void@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-03-16 06:28:55 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/web@8313 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- Config/Extension/service.config.php
+++ Config/Extension/service.config.php
@@ -5,6 +5,6 @@ $config  = array(
 	'2' => array('url' => 'http://api2.chofn.net/', 'token' => '*'),	//超凡网服务接口
 	'3' => array('url' => 'http://tradeapi.chofn.net/', 'token' => '*'), //交易站商品查询接口
     '4' => array('url' => 'http://uc.test.chofn.net/', 'token' => '*'), //用户中心接口
-    '5' => array('url' => 'http://searchapi.chofn.net/', 'token' => '*'),
+    '5' => array('url' => 'http://searchapi.chofn.net/', 'token' => '1d85cf889865f8048849b68bb07fbe23'),
 );
 ?>
