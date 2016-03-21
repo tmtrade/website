6390ca2bbef5eeb3ef2fb7c238ad969c439d21ab
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-03-21 08:57:12 +0000

    提交专题代码
    git-svn-id: http://192.168.0.5/svn/Member/trade/web@8513 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

---
+++ App/Action/topic.action.php
@@ -0,0 +1,27 @@
+<?
+/**
+ * 专题信息
+ *
+ * 专题界面
+ *
+ * @package	Action
+ * @author  far
+ * @since   2016-3-08
+ */
+class TopicAction extends AppAction
+{
+    public $seotime		= '一只蝉';
+    
+    //第一课
+    public function lesson_one()
+    {
+        $this->display();
+    }
+
+    //天猫与京东
+    public function tmall_jd()
+    {
+        $this->display();
+    }
+}
+?>
