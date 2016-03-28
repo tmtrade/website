b1749b4fe922e776e51e05892aa580e3253fb914
far <far@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-03-28 06:12:51 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/web@8878 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Action/topic.action.php
+++ App/Action/topic.action.php
@@ -10,9 +10,9 @@
  */
 class TopicAction extends AppAction
 {
-    public $caches      = array('lesson_one','tmall_jd');
-    public $cacheId     = 'redisHtml';
-    public $expire      = 3600;//1小时
+   // public $caches      = array('lesson_one','tmall_jd');
+   // public $cacheId     = 'redisHtml';
+   // public $expire      = 3600;//1小时
     
     //第一课
     public function lesson_one()
