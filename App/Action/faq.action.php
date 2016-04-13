6bc68c99783d81de45124d150abc6c9af90e2448
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-04-13 09:24:10 +0000

    新闻页面-----带参数访问方式重定向
    git-svn-id: http://192.168.0.5/svn/Member/trade/web@9237 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Action/faq.action.php
+++ App/Action/faq.action.php
@@ -70,9 +70,13 @@ class FaqAction extends AppAction
 	public function news()
 	{
         //解析参数
-        $tag = $this->input('short', 'string', '');
+        //支持以前的访问
         $c = $this->input('c', 'int', 0);
-        $page = $this->input('page', 'int', 0);
+        $page = $this->input('page', 'int', 1);
+        if($c){
+            $this->redirect('',"/n-$c-$page/");
+        }
+        $tag = $this->input('short', 'string', '');
         if ( $tag ){
             if ( strpos($tag, '-') === false ){
                 $c = $tag;
@@ -112,9 +116,12 @@ class FaqAction extends AppAction
         //主动调用时不获取参数
         if($c==0){
             //处理参数---短url或常规url
-            $tag = $this->input('short', 'string', '');
             $c = $this->input('c', 'int', 0);
             $id = $this->input('id', 'int', 0);
+            if($c){
+                $this->redirect('',"/v-$c-$id/");
+            }
+            $tag = $this->input('short', 'string', '');
             if ( $tag ){
                 if ( strpos($tag, '-') === false ) $this->redirect('未找到页面', '/index/error');
                 list($c, $id) = explode('-', $tag);
