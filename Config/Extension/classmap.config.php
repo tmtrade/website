e475b49c299365eb363d40aa3744b66ff6d34759
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-06-16 03:25:49 +0000

    修改登录--站内信相关
    git-svn-id: http://192.168.0.5/svn/Member/trade/web@10367 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- Config/Extension/classmap.config.php
+++ Config/Extension/classmap.config.php
@@ -4,7 +4,6 @@ return array(
 	'Bi'        => BiDir.'/bi.php',
 	'AppForm'   => FormDir.'/app.form.php',
 	'AppAction' => ActionDir.'/app.action.php',
-	'SellerAction' => ActionDir.'/seller/seller.action.php',//出售者平台的类地图
 	'AppModel'  => ModelDir.'/app.model.php',
 	'AppModule' => ModuleDir.'/app.module.php',
 	'QueueCommonAction' => ActionDir.'/queuecommon.action.php',
