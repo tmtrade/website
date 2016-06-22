a2bfb435a074ce17c5c8ee1c6f5440bb7724c26d
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-06-22 03:30:32 +0000

    修改一些报错
    git-svn-id: http://192.168.0.5/svn/Member/trade/web@10451 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Action/app.action.php
+++ App/Action/app.action.php
@@ -45,7 +45,7 @@ abstract class AppAction extends Action
         $this->set('nav_name',$nav_name);
         //设置用户信息
         $this->setLoginUser();
-
+        $this->set('is_index',false);
         //获得热搜数据
         $hotwords = $this->load('index')->getHotWords();
         $this->set('hotwords',$hotwords);
@@ -222,7 +222,7 @@ abstract class AppAction extends Action
      */
     protected function getNavType(){
         $tmp = '';
-        if($url=='/'){
+        if($_SERVER['REQUEST_URI']=='/'){
             $tmp = 'index';
         }elseif(strpos($this->mod,'search') !== false){
             $tmp = 'search';
